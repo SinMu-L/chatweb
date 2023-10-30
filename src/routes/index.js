995dcbd8e19a371a5355b5de674a082b3fb1de89
sinmu <3038267725@qq.com>
2023-10-30 15:55:57 +0800

    使用hash路由

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router"
 import routes from "./routers.js"
 import {store} from '../store.js'
 var router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
