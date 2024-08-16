f14ee302ddd51650505a2968b84c378acf49b368
sinmu <3459917374@qq.com>
2024-08-17 03:28:46 +0800

    美化home页面

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>Home</div>
-    <div>
-        <router-link to="/chat/">start</router-link>
-    </div>
+
+
+
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="text-center">
+
+            <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">模仿<a
+                    href="https://github.com/Chanzhaoyu/chatgpt-web" class=" text-blue-700 ">chatgpt-web</a>前端页面</h1>
+            <p class="mt-6 text-base leading-7 text-gray-600">模仿<a
+                    href="https://github.com/Chanzhaoyu/chatgpt-web">chatgpt-web</a>前端页面</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+
+                <router-link to="/chat/"
+                    class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Quick
+                    Start</router-link>
+            </div>
+        </div>
+    </main>
+
+
+
 </template>
